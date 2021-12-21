60d63b155d3b9caa8c841c6f1cb98d3b334c6cc3
out-shashinov1-vs <satndupic87@gmail.com>
2021-12-21 16:09:01 +0300

    add menu

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,12 +1,33 @@
 import NextImage from 'next/image';
 import {Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay} from '@chakra-ui/layout'
+import NexLink from 'next/link'
+
 import {
     MdHome,
     MdSearch,
     MdLibraryMusic,
     MdPlaylistPlay,
-    MdFavorite
+    MdFavorite, MdMuseum,
 } from 'react-icons/md'
+
+const NavMenu = [
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: '/'
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: '/Search'
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: '/library'
+    }
+]
+
 const SideBar = () => {
     return (
         <Box 
@@ -17,7 +38,23 @@ const SideBar = () => {
             color="gray">
             <Box paddingY="20px">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
-                    <NextImage src="/logo.svg" height={60} width={120}/>
+                    <NextImage src="/logo.svg" height={60} width={120} />
+                </Box>
+                <Box marginBottom={"20px"}>
+                    <List spacing={2}>
+                        {NavMenu.map(menu => (
+                            <ListItem paddingX={"20px"} fontSize={"16px"} key={menu.name}>
+                                <LinkBox>
+                                    <NexLink href={menu.route} passHref>
+                                        <LinkOverlay>
+                                            <ListIcon as={menu.icon} color='white' marginRight="20px"/>
+                                            {menu.name}
+                                        </LinkOverlay>
+                                    </NexLink>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
+                    </List>
                 </Box>
             </Box>
         </Box>
