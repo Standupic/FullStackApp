1d7835d1d731c0b8880ffa0363fe96fa89bb84b2
out-shashinov1-vs <satndupic87@gmail.com>
2022-01-18 17:03:12 +0300

    check git token

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,7 +5,6 @@ import prisma from "./prisma";
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
-
     if (token) {
       let user;
       try {
